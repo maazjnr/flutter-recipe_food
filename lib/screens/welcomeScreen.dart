588396f05e545d1d12c450a355e11a10665cc282
maazjnr <goalmaaz@gmail.com>
2023-04-19 15:46:21 +0100

    updates

---
+++ lib/screens/welcomeScreen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/welcome.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const Center(
+            child: Text(
+              'Your app content goes here',
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
