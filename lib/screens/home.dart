81be6807feca89a55afb6fd3351bb0bae10f620a
maazjnr <goalmaaz@gmail.com>
2023-04-21 00:47:53 +0100

    updaes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,15 +10,42 @@ class Home extends StatelessWidget {
     return Scaffold(
       body: Container(
         child: Column(
-          children: const [
-            SizedBox(height: defaultSpacing * 3,),
-            ListTile(
-              leading: Text('Find Food', style: TextStyle(
-                fontSize: defaultSpacing,
-                fontWeight: FontWeight.w600,
-                color: primaryLight
-              ),),
+          children: [
+            const SizedBox(
+              height: defaultSpacing * 3,
+            ),
+            const ListTile(
+              leading: Text(
+                'Find Food',
+                style: TextStyle(
+                    fontSize: defaultSpacing,
+                    fontWeight: FontWeight.w600,
+                    color: primaryLight),
+              ),
               trailing: Icon(Icons.notifications),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: defaultSpacing * 1.3),
+              child: Row(
+                children: [
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Category",
+                        style: TextStyle(
+                            fontSize: defaultRadius * 1.5, color: primaryDark),
+                      )),
+                  Spacer(),
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Ingredient",
+                        style: TextStyle(
+                            fontSize: defaultRadius * 1.5, color: primaryDark),
+                      )),
+                ],
+              ),
             )
           ],
         ),
